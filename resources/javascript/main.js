94ec8de483b88908095c9a44458790fe9e101033
MateuszKitzol <mateusz@kitzol.pl>
2022-06-17 21:10:42 +0200

    Add responsiveness to the website

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -6,43 +6,37 @@ let radioButtonUp = document.getElementById('radio_button_up');
 let memoryButtonUp = document.getElementById('memory_button_up')
 
 let ramartClickDown = () => {
-    document.getElementById('ramart_image').style.display = "block";
-    document.getElementById('ramart_text').style.display = "block";
+    document.getElementById('ramart_content').style.display = "block";
     ramartButtonDown.style.display = "none";
     ramartButtonUp.style.display = "inline";
 }
 
 let radioClickDown = () => {
-    document.getElementById('radio_image').style.display = "block";
-    document.getElementById('radio_text').style.display = "block";
+    document.getElementById('radio_content').style.display = "block";
     radioButtonDown.style.display = "none";
     radioButtonUp.style.display = "inline";
 }
 
 let memoryClickDown = () => {
-    document.getElementById('memory_image').style.display = "block";
-    document.getElementById('memory_text').style.display = "block";
+    document.getElementById('memory_content').style.display = "block";
     memoryButtonDown.style.display = "none";
     memoryButtonUp.style.display = "inline";
 }
 
 let ramartClickUp = () => {
-    document.getElementById('ramart_image').style.display = "none";
-    document.getElementById('ramart_text').style.display = "none";
+    document.getElementById('ramart_content').style.display = "none";
     ramartButtonDown.style.display = "inline";
     ramartButtonUp.style.display = "none";
 }
 
 let radioClickUp = () => {
-    document.getElementById('radio_image').style.display = "none";
-    document.getElementById('radio_text').style.display = "none";
+    document.getElementById('radio_content').style.display = "none";
     radioButtonDown.style.display = "inline";
     radioButtonUp.style.display = "none";
 }
 
 let memoryClickUp = () => {
-    document.getElementById('memory_image').style.display = "none";
-    document.getElementById('memory_text').style.display = "none";
+    document.getElementById('memory_content').style.display = "none";
     memoryButtonDown.style.display = "inline";
     memoryButtonUp.style.display = "none";
 }
